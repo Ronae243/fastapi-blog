d1bed4aaf70b90ebaf8d37f69e2550c2b41d9105
Ronae243 <ronae2004@gmail.com>
2023-07-18 16:17:45 -0500

    Implemented Schema and hashing and Dependency injection

--- backend/db/base_class.py
+++ backend/db/base_class.py
@@ -1,10 +1,11 @@
 from typing import Any 
-from sqlalchemy.ext.declarative import as_declarative,declared_attr
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 @as_declarative()
 class Base:
     id : Any
     _name_: str
 
-    def _tablename_(cls)->str:
-        return cls._name_.lower()
+    @declared_attr
+    def __tablename__(cls)->str:
+        return cls.__name__.lower()
